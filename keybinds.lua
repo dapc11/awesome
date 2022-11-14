fc3c3c0a50d8daae59ee9c6a36a27f27fe3dfbe4
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-11-14 08:39:23 +0100

    Fix geometry of scratch term

--- keybinds.lua
+++ keybinds.lua
@@ -12,7 +12,7 @@ local term_scratch = bling.module.scratchpad({
   sticky = true, -- Whether the scratchpad should be sticky
   autoclose = true, -- Whether it should hide itself when losing focus
   floating = true, -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
-  geometry = { x = 0, y = 0, height = 900, width = 100% }, -- The geometry in a floating state
+  geometry = { x = 0, y = 0, height = 900, width = 1200 }, -- The geometry in a floating state
   reapply = true, -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
   dont_focus_before_close = false, -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
 })
