c65887b82877960eb80d3c55455ccbaa15b3301a
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-11-15 07:22:49 +0100

    Add debug utility

--- utils.lua
+++ utils.lua
@@ -6,4 +6,24 @@ function M.sleep(n) -- seconds
   end
 end
 
+function M.dump(o)
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) == "boolean" then
+        k = tostring(k)
+      end
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      elseif type(o) == "string" then
+        return tostring('"' .. o .. '"')
+      end
+      s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
+end
+
 return M
